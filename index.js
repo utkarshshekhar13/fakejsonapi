71865ab459f7c3497b2b9ea96c654b5a178c6f4b
Utkarsh Shekhar <utki.619@icloud.com>
2021-09-30 11:01:23 +0530

    Basic Express Setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+express.urlencoded({ extended: false });
+express.json();
+
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "This is home route" });
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
